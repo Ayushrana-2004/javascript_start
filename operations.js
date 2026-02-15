ba5c6f95d9a52685654632e465ea693e6cde08d6
Ayush Rana <157092943+Ayushrana-2004@users.noreply.github.com>
2026-02-15 16:41:13 +0000

    operations

---
+++ operations.js
@@ -0,0 +1,11 @@
+let score ="33"
+console.log(typeof(score))
+//string
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+// number
+
+let isLoggedIn =1
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolenIsLoggedIn); //true
