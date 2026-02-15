a791418365d70fe4300b2dc7726551cf90cac248
Ayush Rana <157092943+Ayushrana-2004@users.noreply.github.com>
2026-02-15 14:38:41 +0000

    checked var const and let

--- test.js
+++ test.js
@@ -1,1 +1,22 @@
-console.log("Ayush")
+// console.log("Ayush")
+const accountId = 1442  //changes can't be done
+let accountEmail = "addyhope958@gmail.com" 
+var accountPass = "hello"
+accountCity = "Gurgaon"
+accountEmail="arana.ddd@gmail.com"
+accountPass="313133"
+accountCity="Chandigarh"
+let accountState
+console.log(accountId)
+
+/*
+Don't use var for declaring a variable 
+*/
+console.table([accountEmail,accountId,accountPass,accountCity,accountState])
+/* (index) │ Values                │
+├─────────┼───────────────────────┤
+│ 0       │ 'arana.ddd@gmail.com' │
+│ 1       │ 1442                  │
+│ 2       │ '313133'              │
+│ 3       │ 'Chandigarh'          │
+│ 4       │ undefined             |*/
