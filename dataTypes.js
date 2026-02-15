0081ee33f1f78da0d68672747e680588197d35cf
Ayush Rana <157092943+Ayushrana-2004@users.noreply.github.com>
2026-02-15 16:26:04 +0000

    datatypes done

---
+++ dataTypes.js
@@ -0,0 +1,10 @@
+//alert("hello") //we are using node.js not browser
+let name ="Ayush"       //string
+let age=21              //int
+let isLoggedIn = true   //boolean
+//null => standalone value
+//undefined 
+//symbol    to find uniqueness
+//objects
+console.log(typeof(name))
+console.log(typeof(null)) //objects
